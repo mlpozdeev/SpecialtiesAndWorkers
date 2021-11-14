dd8113579e792617eaca2ac98c6f46ec0003f47d
Maksim Pozdeev <84346652+mlpozdeev@users.noreply.github.com>
2021-11-14 13:22:52 +0400

    network dagger  with retrofit object created

--- app/src/main/java/com/example/specialtiesandworkers/application/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/specialtiesandworkers/application/di/ApplicationComponent.kt
@@ -1,7 +1,9 @@
 package com.example.specialtiesandworkers.application.di
 
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [ContextModule::class])
 interface ApplicationComponent {
 
---
+++ app/src/main/java/com/example/specialtiesandworkers/data/network/di/NetworkModule.kt
@@ -0,0 +1,27 @@
+package com.example.specialtiesandworkers.data.network.di
+
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    companion object {
+        private const val BASE_URL = "https://gitlab.65apps.com"
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitService(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+}
